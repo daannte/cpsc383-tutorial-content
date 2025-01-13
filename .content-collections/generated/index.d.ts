772116b351476c645e8ccaa23b30d25262f166d4
Dante <85536418+daannte@users.noreply.github.com>
2025-01-12 22:04:07 -0700

    Delete .content-collections directory

--- .content-collections/generated/index.d.ts
+++
@@ -1,7 +0,0 @@
-import configuration from "../../content-collections.ts";
-import { GetTypeByName } from "@content-collections/core";
-
-export type Tutorial = GetTypeByName<typeof configuration, "Tutorials">;
-export declare const allTutorials: Array<Tutorial>;
-
-export {};
